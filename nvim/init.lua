a812ad46be898f629cde104c75c6d3e0f06f40d0
teslapop <teslappop9090@gmail.com>
2025-08-22 17:02:15 +0530

    updated nvim and zshrc

---
+++ nvim/init.lua
@@ -0,0 +1,126 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Theme (Catppuccin with transparency)
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
+        transparent_background = true,
+        integrations = {
+          nvimtree = true,
+          treesitter = true,
+          lsp_trouble = true,
+          cmp = true,
+          mason = true,
+          lualine = true,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end
+  },
+
+  -- File Explorer (with keybinding)
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        renderer = {
+          highlight_opened_files = "name",
+          highlight_git = true,
+        }
+      })
+      vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+    end
+  },
+
+  -- Status Line
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        options = { theme = "catppuccin" }
+      })
+    end
+  },
+
+  -- LSP + Mason
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim"
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+      require("lspconfig").lua_ls.setup {}
+    end
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip"
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = {
+          { name = "nvim_lsp" }
+        }
+      })
+    end
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true }
+      })
+    end
+  }
+})
+
+-- General settings after Lazy setup
+vim.opt.number = true          -- Show line numbers
+vim.opt.relativenumber = true  -- Relative line numbers
+vim.opt.signcolumn = "no"      -- Hide sign column
+vim.opt.clipboard:append("unnamedplus")
+
+-- Ensure full transparency for all UI elements
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NormalNC guibg=none
+  highlight SignColumn guibg=none
+  highlight NormalFloat guibg=none
+  highlight NvimTreeNormal guibg=none
+  highlight NvimTreeNormalNC guibg=none
+]])
+
